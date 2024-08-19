96315ce5d1d4c70511e53deebf5eb8487617cebc
yunsung hong <45896385+fuzzy0260@users.noreply.github.com>
2024-08-19 10:30:51 +0900

    .

--- cn/terraform/cloudwatch.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_cloudwatch_log_group" "vpc_flow_log_group" {
-  name = "ma-vpc-flow-log"
-}
--- se/terraform/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "ap-northeast-2"
-}
-
-provider "aws" {
-  region = "us-east-1"
-  alias = "verginia"
-}
