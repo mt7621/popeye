a47f6584b7475bac2dd65e29041cc97c99f9a2eb
yunsung hong <45896385+fuzzy0260@users.noreply.github.com>
2024-08-19 09:46:18 +0900

    .

---
+++ SE/src/tables.sql
@@ -0,0 +1,11 @@
+create table product(
+	id VARCHAR(255) PRIMARY KEY,
+	name VARCHAR(255),
+	category VARCHAR(255)
+);
+
+create table customer(
+	id VARCHAR(255) PRIMARY KEY,
+	name VARCHAR(255),
+	gender VARCHAR(255)
+);
